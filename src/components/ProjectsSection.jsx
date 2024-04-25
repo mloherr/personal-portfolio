ebffc8efe40d3ab5ee05cafb1d8000c54ea194ed
mloherr <herraizmaria08@gmail.com>
2024-04-25 20:20:06 +0200

    Created new styles and schema for projects section on mobile version

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -2,32 +2,23 @@ import '../scss/components/ProjectsSection.scss';
 import '../scss/core/variables.scss';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import quijote from '../images/quijote.jpg';
-import fantasma from '../images/Fantasma.png';
-import web from '../images/web.png';
 
 function ProjectsSection() {
   return (
     <section className="projectsSection">
       <Link to="/graphic-design-1">
         <div className="projectsSection__cardDesign">
-          <figure className="projectsSection__cardDesign--background">
-            <img src={quijote} alt="Design for JCCM tourist center" />
-          </figure>
+          <div className="projectsSection__cardDesign--background"></div>
           <p className="projectsSection__cardDesign--text">DISEÑO GRÁFICO</p>
         </div>
       </Link>
-      <div className="projectsSection__cardIlustration">
-        <figure className="projectsSection__cardIlustration--background">
-          <img src={fantasma} alt="Digital drawing for the spooky season" />
-        </figure>
-        <p className="projectsSection__cardIlustration--text">ILUSTRACIÓN</p>
-      </div>
       <div className="projectsSection__cardProgramming">
-        <figure className="projectsSection__cardProgramming--background">
-          <img src={web} alt="Digital drawing for the spooky season" />
-        </figure>
+        <div className="projectsSection__cardProgramming--background"></div>
         <p className="projectsSection__cardProgramming--text">DESARROLLO WEB</p>
+      </div>
+      <div className="projectsSection__cardIlustration">
+        <div className="projectsSection__cardIlustration--background"></div>
+        <p className="projectsSection__cardIlustration--text">ILUSTRACIÓN</p>
       </div>
     </section>
   );
