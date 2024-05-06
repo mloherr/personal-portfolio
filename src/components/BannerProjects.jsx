a9d4f3eb9850ff9196aa8f6bc94ac5e6ae986334
mloherr <herraizmaria08@gmail.com>
2024-05-06 13:55:58 +0200

    Created media queries for banner component

--- src/components/BannerProjects.jsx
+++ src/components/BannerProjects.jsx
@@ -2,12 +2,16 @@ import '../scss/components/BannerProjects.scss';
 import '../scss/core/variables.scss';
 
 function BannerProjects({ items }) {
+  const marginBanner = window.innerWidth >= 726 ? 'mx-20' : 'mx-10';
+  const textSize = window.innerWidth >= 726 ? 'text-6xl' : 'text-4xl';
+
+  const combinedClassNames = `${marginBanner} ${textSize}`;
   return (
     <div className="relative flex w-full overflow-x-hidden  titleSection">
       <div className="animate-marquee whitespace-nowrap py-6 titleSection__text">
         {items.map((item) => {
           return (
-            <span key={item} className="mx-10 text-4xl">
+            <span key={item} className={combinedClassNames}>
               {item}
             </span>
           );
@@ -17,7 +21,7 @@ function BannerProjects({ items }) {
       <div className="absolute top-0 animate-marquee2 whitespace-nowrap py-6 titleSection__text">
         {items.map((item) => {
           return (
-            <span key={item} className="mx-10 text-4xl">
+            <span key={item} className={combinedClassNames}>
               {item}
             </span>
           );
